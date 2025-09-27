fd75769948ec3ffeea1096cc849ced02705254ba
Jose Cueva <josemcz2211@gmail.com>
2025-09-26 21:21:12 -0500

    Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/main/java/com/agenda/agenda_inteligente/AgendaInteligenteApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.agenda_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaInteligenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenda/agenda_inteligente/AgendaInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.agenda_inteligente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
