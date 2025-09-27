72022255efefbdc906d2abeb58c3caad509600a2
Jose Cueva <josemcz2211@gmail.com>
2025-09-27 01:25:47 -0500

    Agregar anotación CrossOrigin en AuthController para permitir solicitudes CORS

--- src/main/java/com/agenda/agenda_inteligente/interfaces/rest/AuthController.java
+++ src/main/java/com/agenda/agenda_inteligente/interfaces/rest/AuthController.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     private final AutenticarUsuario autenticarUsuario;
