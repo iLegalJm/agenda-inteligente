5b65d48ad470d39919614ccb8e4cc6db43273e4b
Jose Cueva <josemcz2211@gmail.com>
2025-09-28 21:42:13 -0500

    Eliminar clases y archivos relacionados con la autenticación y registro de usuarios para simplificar la estructura del proyecto

--- src/main/java/com/agenda/agenda_inteligente/infrastructure/persistence/JpaUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agenda.agenda_inteligente.infrastructure.persistence;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaUsuarioRepository extends JpaRepository<JpaUsuarioEntity, Long> {
-    Optional<JpaUsuarioEntity> findByEmail(String email);
-}
